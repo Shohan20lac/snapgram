95346ad8454b1ac4cdd6d5d8a45b81b2e161b8ff
shohan20lac <r.shohan12@gmail.com>
2023-10-29 21:40:59 +0600

    imported theming information to tailwind.config.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <h1 className="text-3xl font-bold underline">
+        Hello Snapgram!
+    </h1>
   )
 }
 
