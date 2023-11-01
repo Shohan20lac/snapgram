07d42edb730f42772d88af377e0fcbc36bc443ed
shohan20lac <r.shohan12@gmail.com>
2023-11-01 18:48:29 +0600

    add tailwindclass to autthlayout

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -12,7 +12,7 @@ const isAuthenticated = false;
         )
         : (
           <>
-            <section>
+            <section className="flex flex-1 justify-center items-center flex-col py-10">
               <Outlet/>
             </section>
           </>
